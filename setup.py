1c5c2c23802af38a298589845d7c060b60885f2e
mantvmass <kliop2317@gmail.com>
2024-02-15 03:43:04 +0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="ldplayer",
+    version="0.0.1",
+    description="This is package for LDPlayer emulator control software. (unofficial)",
+    author="mantvmass",
+    maintainer="Phumin Maliwan",
+    maintainer_email="kliop2317@gmail.com",
+    url="https://github.com/mantvmass/ldplayer",
+    packages=["ldplayer"],
+    long_description=open("README.md").read(),
+    install_requires=[]
+)
